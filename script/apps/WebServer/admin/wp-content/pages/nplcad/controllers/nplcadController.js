20041df0b6256a77cb3b1e85fc5a6f7710701291
leio zhang <leio zhang>
2016-07-13 16:59:37 +0800

    refine nplcad folder's structrue

--- script/apps/WebServer/admin/wp-content/pages/nplcad/controllers/nplcadController.js
+++ script/apps/WebServer/admin/wp-content/pages/nplcad/controllers/nplcadController.js
@@ -78,16 +78,104 @@ angular.module('NPLCAD_App', ['ngStorage'])
         var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         last_mesh = new THREE.Mesh(geometry, material);
         editor.addObject(last_mesh);
+        geometry = new THREE.Geometry().fromBufferGeometry(geometry);
+        return geometry;
+    }
+    function stlFromGeometry(geometry, options) {
+
+        // calculate the faces and normals if they are not yet present
+        geometry.computeFaceNormals()
+
+        var addX = 0
+        var addY = 0
+        var addZ = 0
+        var download = false
+
+        if (options) {
+            if (options.useObjectPosition) {
+                addX = geometry.mesh.position.x
+                addY = geometry.mesh.position.y
+                addZ = geometry.mesh.position.z
+            }
+
+            if (options.download) {
+                download = true
+            }
+        }
+
+
+        var facetToStl = function (verts, normal) {
+            var faceStl = ''
+            faceStl += 'facet normal ' + normal.x + ' ' + normal.y + ' ' + normal.z + '\n'
+            faceStl += 'outer loop\n'
+
+            for (var j = 0; j < 3; j++) {
+                var vert = verts[j]
+                faceStl += 'vertex ' + (vert.x + addX) + ' ' + (vert.y + addY) + ' ' + (vert.z + addZ) + '\n'
+            }
+
+            faceStl += 'endloop\n'
+            faceStl += 'endfacet\n'
+
+            return faceStl
+        }
+
+        // start bulding the STL string
+        var stl = ''
+        stl += 'solid\n'
+
+        for (var i = 0; i < geometry.faces.length; i++) {
+            var face = geometry.faces[i]
+
+            // if we have just a griangle, that's easy. just write them to the file
+            if (face.d === undefined) {
+                var verts = [
+                    geometry.vertices[face.a],
+                    geometry.vertices[face.b],
+                    geometry.vertices[face.c]
+                ]
+
+                stl += facetToStl(verts, face.normal)
+
+            } else {
+                // if it's a quad, we need to triangulate it first
+                // split the quad into two triangles: abd and bcd
+                var verts = []
+                verts[0] = [
+                    geometry.vertices[face.a],
+                    geometry.vertices[face.b],
+                    geometry.vertices[face.d]
+                ]
+                verts[1] = [
+                    geometry.vertices[face.b],
+                    geometry.vertices[face.c],
+                    geometry.vertices[face.d]
+                ]
+
+                for (var k = 0; k < 2; k++) {
+                    stl += facetToStl(verts[k], face.normal)
+                }
+
+            }
+        }
+
+        stl += 'endsolid'
+
+        if (download) {
+            document.location = 'data:Application/octet-stream, ' + encodeURIComponent(stl)
+        }
+
+        return stl
     }
     var code_editor = ace.edit("code_editor");
     code_editor.setTheme("ace/theme/github");
     code_editor.getSession().setMode("ace/mode/lua");
     code_editor.setShowPrintMargin(false);
 
-    $scope.onRunCode = function () {
+    $scope.onRunCode = function (bSave) {
         $("#logWnd").html("");
         var text = code_editor.getValue();
-        $http.get("ajax/nplcad?action=runcode&code=" + encodeURIComponent(text)).then(function (response) {
+        $http.get("ajax/nplcad2?action=runcode&code=" + encodeURIComponent(text)).then(function (response) {
             if (response && response.data && response.data.result) {
                 if (response.data.success) {
                     var vertices = response.data.result[0];
@@ -95,7 +183,10 @@ angular.module('NPLCAD_App', ['ngStorage'])
                     var normals = response.data.result[2];
                     var colors = response.data.result[3];
 
-                    createMesh(vertices, indices, normals, colors);
+                    var geometry = createMesh(vertices, indices, normals, colors);
+                    if (bSave) {
+                        stlFromGeometry(geometry, { download: true })
+                    }
 
                 }else{
                     $("#logWnd").html(response);
@@ -106,6 +197,5 @@ angular.module('NPLCAD_App', ['ngStorage'])
             }
         });
     }
-
     onWindowResize();
 });
