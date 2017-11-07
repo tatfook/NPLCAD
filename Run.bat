668212c76cf79f880d048d9b3fc1c7dc37b1becd
LiXizhi <lixizhi@yeah.net>
2017-11-07 14:56:22 +0800

    fix run script

--- Run.bat
+++ Run.bat
@@ -1,4 +1,8 @@
 @echo off 
+if exist "%~dp0../../redist/ParaEngineClient.exe" (
 pushd "%~dp0../../redist/" 
+) else (
+pushd "%~dp0../../" 
+)
 call "ParaEngineClient.exe" single="false" mc="true" noupdate="true" dev="%~dp0" mod="NPLCAD" isDevEnv="true" world="worlds/DesignHouse/test"
 popd 
